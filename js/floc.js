23fb15bd962c98dc0f29a4fcdb4af59bddf7ebe6
Abhinav Sinha <abhinavsinha001@gmail.com>
2021-03-08 21:20:12 +0530

    Update floc.js

--- js/floc.js
+++ js/floc.js
@@ -1,5 +1,7 @@
 export function getFlocId() {
   if(typeof typeof document.interestCohort !== "undefined"){
     document.interestCohort().then(data => console.alert(data)); 
+  }else{
+    console.alert("FLoC feature not available/enabled on browser")
   }
 }
