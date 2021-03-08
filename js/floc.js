a9b00b9204261415d482ecb4b9159825436ac622
Abhinav Sinha <abhinavsinha001@gmail.com>
2021-03-08 23:22:06 +0530

    Update floc.js

--- js/floc.js
+++ js/floc.js
@@ -1,8 +1,34 @@
 function getFlocId() {
   if(typeof document.interestCohort !== "undefined"){
-    document.interestCohort().then(data => window.alert(data)); 
+    document.interestCohort().then(data => storeFlocId(data));
+    displayFlocIds();
   }else{
     window.alert("FLoC feature not available/enabled on browser")
   }
 }
+
+function storeFlocId(data){
+  var oldIds = JSON.parse(localStorage.getItem('flocId')) || [];
+  if oldIds.includes(data){
+    
+  }else{
+    console.log("FLoC ID not updated:"+data);
+  }else{
+    oldIds.push(data)
+    localStorage.setItem("flocId",data)
+  }
+}
+
+function displayFlocIds(){
+  var flocIds = JSON.parse(localStorage.getItem('flocId')) || [];
+  var html = "<table border='1|1'>";
+  for (var i = 0; i < flocIds.length; i++) {
+      html+="<tr>";
+      html+="<td>"+flocIds+"</td>";
+      html+="</tr>";
+  }
+  html+="</table>";
+  document.getElementById("box").innerHTML = html;
+}
+
 getFlocId();
